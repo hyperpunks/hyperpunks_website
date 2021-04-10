07294d86e464a7bfecf9eef29ad0a0561c75dad3
hyperpunks-nft <81994174+hyperpunks-nft@users.noreply.github.com>
2021-04-10 14:52:07 +0100

    Update ar.vue

--- pages/ar.vue
+++ pages/ar.vue
@@ -1,7 +1,9 @@
 <template>
+ 
   <v-row class="mt-5" justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
+      <iframe src="https://player.vimeo.com/video/534823039" width="640" height="296" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+       <div class="text-center">
         AR Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut
         doloremque fugiat dolorem eligendi consequatur quas ab, ducimus
         voluptatum iure mollitia modi, dolorum delectus accusamus aspernatur

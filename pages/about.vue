7355c79972bd5093bf676d0ee8838aa935e6bfb2
hyperpunks-nft <81994174+hyperpunks-nft@users.noreply.github.com>
2021-04-10 18:19:39 +0100

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -3,10 +3,12 @@
   <v-row class="mt-5" justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut doloremque
-        fugiat dolorem eligendi consequatur quas ab, ducimus voluptatum iure
-        mollitia modi, dolorum delectus accusamus aspernatur ipsa temporibus
-        libero, pariatur sapiente?
+        <iframe class="aligncenter" src="https://player.vimeo.com/video/534832934" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut doloremque
+          fugiat dolorem eligendi consequatur quas ab, ducimus voluptatum iure
+          mollitia modi, dolorum delectus accusamus aspernatur ipsa temporibus
+          libero, pariatur sapiente?
+        </iframe>
       </div>
     </v-col>
   </v-row>

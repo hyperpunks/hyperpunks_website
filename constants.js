9ce0d82df10552d953376c8da80db2455b10f57e
x <x@to.wtf>
2021-04-12 18:10:39 +0300

    :zap:

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
-export const DEADLINE = '04/12/2021 10:18:00' // 11th of April
+export const DEADLINE = '04/12/2021 15:12:00 GMT'
 export const CONTRACT_ADDR = '0x811dFfbb08eDA814a6591bb6f5869Cc55571978e'
 export const NETWORK_ID = 4
 export const RPC_PROVIDER =
   'https://rinkeby.infura.io/v3/870526d757ed4ab29be2c48bbc625e05'
-//   'https://eth-mainnet.alchemyapi.io/v2/mtVlVYO3Hbs10efz3tyQpeG6pAYVBogM'
+//https://eth-mainnet.alchemyapi.io/v2/mtVlVYO3Hbs10efz3tyQpeG6pAYVBogM

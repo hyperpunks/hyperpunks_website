21cd1ceb97b9d1ac6a0223d22e35937a3813e002
Andrei <andrei@coinmode.com>
2021-04-12 10:34:33 +0300

    :zap:

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 export const DEADLINE = '04/12/2021 10:18:00' // 11th of April
-export const CONTRACT_ADDR = '0x3822DE478b708c39cd31FfE834dED5603d244643'
+export const CONTRACT_ADDR = '0x811dFfbb08eDA814a6591bb6f5869Cc55571978e'
 export const NETWORK_ID = 4
 export const RPC_PROVIDER =
   'https://rinkeby.infura.io/v3/870526d757ed4ab29be2c48bbc625e05'
